a0c9781189ab59a3bda3320d99165b26f0205c48
Kostis <giokostis@gmail.com>
2015-02-07 20:22:05 +0200

    Menu levels showing - need text

--- Assets/Scripts/Managers/MainMenu.cs
+++ Assets/Scripts/Managers/MainMenu.cs
@@ -3,8 +3,31 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 
+
+	public GameObject lvlPrefab;
+	public int maxLevel = 12;
+	public int collumns = 5;
+	
 	void Start () 
 	{
-		Debug.Log("I am a happy Main Menu!");
+		int i;
+		float x = -2f;
+		float y = 1f;
+		for(i=0; i<maxLevel; i++) {
+
+			if(i%5 == 0) {
+				y-=0.6f;
+				x=-1.8f;
+			}
+			x+=0.6f;
+			GameObject newLevel = (GameObject)Instantiate(lvlPrefab, new Vector3 (x,y,0), Quaternion.identity);
+			newLevel.transform.localScale = new Vector3(0.1f,0.1f,0.1f);
+			//((GameObject)Instantiate(lvlPrefab,lvlPos.transform.position,Quaternion.identity)).GetComponent<Lvl>();
+			
+			print ("Created a level");
+		}
+	
 	}
+
+
 }
